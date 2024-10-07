9c476215ffbb6d2de6f9b1d07b23a5766b80b6fa
mikuru <153439700+Overionised@users.noreply.github.com>
2024-10-07 09:50:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -101,8 +101,9 @@ int main() {
             cout << "x1 se rovna = " << x1 << "\n x2 se rovna = " << x2 << "\n";
         }
     }
-    else if (b != 0 && c >= 0){ x1 = -c/b; silly();cout<< "x se rovna : "<<x1<<"rovnice je linearni"<<endl;  }
+    else if (b != 0 && c > 0){ x1 = -c/b; silly();cout<< "x se rovna : "<<x1<<" rovnice je linearni"<<endl;  }
     else if ((b == 0) && (c == 0)){ cout<<"nekonecne mnoho reseni"<<endl;}
+    else if ( c == 0){cout<< "x se rovna : 0";}
     else{cout<<"nema reseni \n";}
 	return 0;
 }
