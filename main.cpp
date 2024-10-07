ef0244f11b8e87ae4da96f59e31705e13e0966fc
mikuru <153439700+Overionised@users.noreply.github.com>
2024-10-07 09:27:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -101,6 +101,6 @@ int main() {
             cout << "x1 se rovna = " << x1 << "\n x2 se rovna = " << x2 << "\n";
         }
     }
-    else { x1 = -c/b; cout<< "x se rovna : "<<x1<<"rovnice je linearni"<<endl;  }
+    else { x1 = -c/b; silly();cout<< "x se rovna : "<<x1<<"rovnice je linearni"<<endl;  }
 	return 0;
 }
